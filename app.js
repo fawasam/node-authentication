d1d57440392a2293fdfc6a7990a05bd3f4eda44f
fwzam <fawasam32@gmail.com>
2021-05-12 23:40:29 +0530

    level 3 authentication

--- app.js
+++ app.js
@@ -6,16 +6,21 @@
   //in Save Section the password goes to encrypted
   //in find section the password goes to decrypted
 
+
+  //level 3 security
+  //new dependency for npm i md5
+
+//md5 method
+ //hasing is the main encryption used here  hashing  (register)=>md5(req.body.password)== (login)=>md5(req.body.password)
+
   require("dotenv").config(); 
 const express = require("express");
 const bodyParse = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
-
-console.log(process.env.SECRET)
 
 
 app.use(express.static("public"));
@@ -30,11 +35,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt , {secret:process.env.SECRET , encryptedFields:["password"]});
-
-
 const User =new mongoose.model("User" ,userSchema); 
-
 
 
 //home
@@ -51,8 +52,8 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
    const newUser =new User({
      email: req.body.username,
-     password: req.body.password
-   })
+     password: md5(req.body.password)
+  })
 
    // Mongoose Model.findOne()
    let errors =[];
@@ -85,7 +86,7 @@ app.get("/login", (req, res) => {
 app.post('/login' ,(req, res) => {
   const username = req.body.username
   
-  const password = req.body.password
+  const password =  md5(req.body.password)
   //in find section the password goes to decrypted
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
