8923210d6f511f5855ef822bbdd675fe40e955f6
fawasam <fawasam5@gmail.com>
2021-05-18 23:26:33 +0530

    level 4 authentication

--- app.js
+++ app.js
@@ -11,7 +11,13 @@
   //new dependency for npm i md5
 
 //md5 method
+//password + hashing = hash password
  //hasing is the main encryption used here  hashing  (register)=>md5(req.body.password)== (login)=>md5(req.body.password)
+
+
+//level 4 security
+ //salting 
+ // password +salting = salted password  or  qwerty + random number (xxxxx) -> hash password
 
   require("dotenv").config(); 
 const express = require("express");
@@ -19,8 +25,10 @@ const bodyParse = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const md5=require("md5");
+// const md5=require("md5");
 
+const bcrypt = require("bcrypt")
+const saltRounds = 10;
 
 
 app.use(express.static("public"));
@@ -50,31 +58,36 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-   const newUser =new User({
-     email: req.body.username,
-     password: md5(req.body.password)
-  })
 
-   // Mongoose Model.findOne()
-   let errors =[];
-   User.findOne({email:req.body.username}).then(user=>{
-    if(user){
-        res.render('register',{errors})
-        console.log("email already exists")
-    }
-   //EMAIL EXIST CHECK SECTION END
-    else{
-       //in Save Section the password goes to encrypted
-      newUser.save( (err, user) => {
-        if (err) {
-          console.log(err);
-        }
-        else{
-          res.render("secrets")
-        }
-      });
-    }
-})
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+    const newUser =new User({
+      email: req.body.username,
+      password: hash
+   })
+ 
+    // Mongoose Model.findOne()
+    let errors =[];
+    User.findOne({email:req.body.username}).then(user=>{
+     if(user){
+         res.render('register',{errors})
+         console.log("email already exists")
+     }
+    //EMAIL EXIST CHECK SECTION END
+     else{
+        //in Save Section the password goes to encrypted
+       newUser.save( (err, user) => {
+         if (err) {
+           console.log(err);
+         }
+         else{
+           res.render("secrets")
+         }
+       });
+     }
+ })
+});
+  
 })
 
 
@@ -85,25 +98,26 @@ app.get("/login", (req, res) => {
 
 app.post('/login' ,(req, res) => {
   const username = req.body.username
-  
-  const password =  md5(req.body.password)
+  const password = req.body.password
   //in find section the password goes to decrypted
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
       console.log(err);
-      alert("credentials have errors please check proper id and password")
+      // alert("credentials have errors please check proper id and password")
+    }else{
+       if(foundUser)
+       {
+         
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true){
+            res.render("secrets")
+            console.log("success")
+          }
+        
+       })
     }
-    else{
-      if (foundUser)
-      {
-        if (foundUser.password === password)
-        {
-          res.render("secrets")
-          console.log("success")
-        }
-      }
-    }
-  })
+  }
+})
 })
 app.listen(3000, function () {
   console.log("hii you are in port 30000");
